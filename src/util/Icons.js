114f5bcce67efaa1658ac003ae26dbe673759c4a
Cylour <72263027+Cylour@users.noreply.github.com>
2024-07-22 00:09:14 +1000

    Fix discord links

--- src/util/Icons.js
+++ src/util/Icons.js
@@ -3,54 +3,54 @@ import { ReactComponent as DiscordIcon } from "../assets/discord-icon.svg";
 import { Box } from "@mui/material";
 
 const IconWithHover = ({ icon, link }) => {
-    return (
-        <Box
-            sx={{
-                marginRight: "5px", // Add some margin between icons
-                transition: "transform 0.2s", // Add transition for smooth hover effect
-                "&:hover": {
-                    transform: "scale(1.2)", // Enlarge the icon by 20% on hover
-                    cursor: "pointer",
-                },
-            }}
-            onClick={() => window.open(link, "_blank")}
-        >
-            {icon}
-        </Box>
-    );
+  return (
+    <Box
+      sx={{
+        marginRight: "5px", // Add some margin between icons
+        transition: "transform 0.2s", // Add transition for smooth hover effect
+        "&:hover": {
+          transform: "scale(1.2)", // Enlarge the icon by 20% on hover
+          cursor: "pointer",
+        },
+      }}
+      onClick={() => window.open(link, "_blank")}
+    >
+      {icon}
+    </Box>
+  );
 };
 
 const SocialMediaIcons = () => {
-    return (
-        <Box
-            sx={{
-                display: "flex",
-                marginBottom: "5px",
-                paddingBottom: "10px",
-            }}
-        >
-            <IconWithHover
-                icon={<Instagram sx={{ fontSize: 35 }} />}
-                link="https://www.instagram.com/unswai.soc/"
-            />
-            <IconWithHover
-                icon={<Facebook sx={{ fontSize: 35 }} />}
-                link="https://www.facebook.com/profile.php?id=100092595608038"
-            />
-            <IconWithHover
-                icon={<GitHub sx={{ fontSize: 35 }} />}
-                link="https://github.com/AISoc-UNSW"
-            />
-            <IconWithHover
-                icon={<LinkedIn sx={{ fontSize: 35 }} />}
-                link="https://www.linkedin.com/company/unsw-artificial-intelligence-society/"
-            />
-            <IconWithHover
-                icon={<DiscordIcon style={{ width: "35px", height: "35px" }} />}
-                link="https://discord.gg/gpahrBHB"
-            />
-        </Box>
-    );
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        marginBottom: "5px",
+        paddingBottom: "10px",
+      }}
+    >
+      <IconWithHover
+        icon={<Instagram sx={{ fontSize: 35 }} />}
+        link="https://www.instagram.com/unswai.soc/"
+      />
+      <IconWithHover
+        icon={<Facebook sx={{ fontSize: 35 }} />}
+        link="https://www.facebook.com/profile.php?id=100092595608038"
+      />
+      <IconWithHover
+        icon={<GitHub sx={{ fontSize: 35 }} />}
+        link="https://github.com/AISoc-UNSW"
+      />
+      <IconWithHover
+        icon={<LinkedIn sx={{ fontSize: 35 }} />}
+        link="https://www.linkedin.com/company/unsw-artificial-intelligence-society/"
+      />
+      <IconWithHover
+        icon={<DiscordIcon style={{ width: "35px", height: "35px" }} />}
+        link="https://discord.gg/856FnEQePJ"
+      />
+    </Box>
+  );
 };
 
 export default SocialMediaIcons;
